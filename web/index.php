1248f92bc565a457adef654653b94ae29ed73c46
umpirsky <umpirsky@gmail.com>
2014-09-25 19:55:14 +0200

    Twig

--- web/index.php
+++ web/index.php
@@ -4,10 +4,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__.'/../config.json'));
+$app->register(new Igorw\Silex\ConfigServiceProvider(
+    __DIR__.'/../config.json'
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/themes',
+));
 
-$app->get('/', function() {
-    return 'Hello Freelander!';
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('default/index.html');
 });
 
 $app->run();
