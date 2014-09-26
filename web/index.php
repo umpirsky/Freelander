913d52f68e12ba40dc340550561e9f3e219176cb
umpirsky <umpirsky@gmail.com>
2014-09-26 12:09:08 +0200

    Initial Mailchimp integration

--- web/index.php
+++ web/index.php
@@ -7,12 +7,27 @@ $app = new Silex\Application();
 $app->register(new Igorw\Silex\ConfigServiceProvider(
     __DIR__.'/../config.json'
 ));
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/themes',
-));
+]);
+
+$app->register(new SilexMailchimp\Provider\MailchimpServiceProvider());
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('default/index.html');
-});
+})->bind('home');
+
+$app->post('/subscribe', function(Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    $app['mailchimp']->lists->subscribe($app['mailchimp.list.id'], array('email' => $request->get('email')));
+
+    $app['session']->getFlashBag()->add('message', 'Thanks for subscribing.');
+
+    return $app->redirect($app['url_generator']->generate('home'));
+})->bind('subscribe');
 
 $app->run();
