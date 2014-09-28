142e5002f36862d4e56dac3a6778bc698f7798d7
umpirsky <umpirsky@gmail.com>
2014-09-28 14:44:19 +0200

    New theme - application

---
+++ web/themes/application/javascript/assets/application.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  /*-----------------------------------------------------------------------------------*/
+  /*  Anchor Link
+  /*-----------------------------------------------------------------------------------*/
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+        return false;
+      }
+    }
+  });
+
+  /*-----------------------------------------------------------------------------------*/
+  /*  Tooltips
+  /*-----------------------------------------------------------------------------------*/
+  $('.tooltip-side-nav').tooltip();
+  
+});
